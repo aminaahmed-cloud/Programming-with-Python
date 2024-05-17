6bf5343d47cee8807125e639e18085d37293b910
Amina Ahmed <148248530+aminaahmed-cloud@users.noreply.github.com>
2024-05-17 14:13:19 +0100

    Add files via upload

---
+++ List of Dictionaries.py
@@ -0,0 +1,31 @@
+# Print out - the name, job and city of each employee using a loop. The program must work for any number of employees in the list, not just 2
+employees = [{
+  "name": "Tina",
+  "age": 30,
+  "birthday": "1990-03-10",
+  "job": "DevOps Engineer",
+  "address": {
+    "city": "New York",
+    "country": "USA"
+  }
+},
+{
+  "name": "Tim",
+  "age": 35,
+  "birthday": "1985-02-21",
+  "job": "Developer",
+  "address": {
+    "city": "Sydney",
+    "country": "Australia"
+  }
+}]
+
+for employee in employees:
+    print(f"name: {employee['name']}")
+    print(f"job: {employee['job']}")
+    print(f"city: {employee['address']['city']}")
+    print("--------------------------")
+#  Prints the country of the second employee in the list by accessing it directly without the loop.
+
+country = employees[1]["address"]["country"]
+print(f"country of the second employee: {country}")
